fb59d2ea7d872a5cb6ea551d96d0c40148774378
FAkinlabi <akinlabifm@yahoo.com>
2020-07-13 04:58:53 +0100

    Changed MongoDB database name to what provided by Heroku

--- src/common/database.py
+++ src/common/database.py
@@ -12,7 +12,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['fullstack']
+        Database.DATABASE = client['heroku_rrk7322c']
 
     @staticmethod
     def insert(collection, data):
